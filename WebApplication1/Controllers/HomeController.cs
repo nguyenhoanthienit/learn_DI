75b2504154c125f4392d09a4d99303d26a3dc7e6
Thien Nguyen <nguyenhoanthienit@gmail.com>
2020-07-01 17:04:45 +0700

    1st commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public IStudentService _service;
+
+        public HomeController(IStudentService studentService)
+        {
+            _service = studentService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_service.GetAll());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Services/StudentService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public interface IStudentService
+    {
+        List<Student> GetAll();
+        Student GetStudent(int id);
+    }
+
+    public class StudentService : IStudentService
+    {
+        public List<Student> GetAll()
+        {
+            return new List<Student>
+            {
+                new Student { ID = 1, Name = "A", Class = "CA"},
+                new Student { ID = 2, Name = "B", Class = "CB"},
+                new Student { ID = 3, Name = "C", Class = "CC"}
+            };
+        }
+
+        public Student GetStudent(int id)
+        {
+            return GetAll().Where(i => i.ID == id).FirstOrDefault();
+        }
+    }
+}
